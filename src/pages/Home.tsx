1178f6019adaf3c11341756883bfbd06f9514b89
DManti15 <dannymantigar@hotmail.com>
2021-11-25 22:35:32 -0600

    removed default attributes and added icon within toolbar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonIcon, IonButton } from '@ionic/react';
+import { menu } from 'ionicons/icons'
 import './Home.scss';
 
 const Home: React.FC = () => {
@@ -7,7 +7,10 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonButton fill="clear">
+            <IonIcon icon={menu}></IonIcon>
+          </IonButton>
+          <IonTitle>Home</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -16,7 +19,6 @@ const Home: React.FC = () => {
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
       </IonContent>
     </IonPage>
   );
