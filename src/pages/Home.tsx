9078d5ab061ae7fd06fdd5279f3b4b88a27cc776
WilfredoHernandez <wilfredo.munguiafb@gmail.com>
2021-11-30 22:30:56 -0600

    Styles and API call updated

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -19,10 +19,33 @@ import { swapHorizontal } from "ionicons/icons";
 import "./Home.scss";
 
 const Home: React.FC = () => {
+
+  let api = "https://api.blockchain.com/v3/exchange/tickers/BTC-USD";
+
+  function myMethod( )
+    {
+      api = "https://api.blockchain.com/v3/exchange/tickers/BTC-USD";
+      fetch(api)
+      .then(response =>{ return response.json();})
+      .then (data =>{
+        console.log(data);
+      });
+    }
+
+  window.addEventListener("load",()=>{
+    api = "https://api.blockchain.com/v3/exchange/tickers/BTC-USD";
+    fetch(api)
+    .then(response =>{ return response.json();})
+    .then (data =>{
+      console.log(data);
+      //setInterval(myMethod, 5000);
+    });
+  });
+
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
+        <IonToolbar className="mainToolBar">
           <IonButton className="toolbarButton" slot="start">
             <IonIcon icon={menu} className="toolbarIcon"></IonIcon>
           </IonButton>
@@ -38,12 +61,12 @@ const Home: React.FC = () => {
                 className="logoBTC"
                 src={"assets/Images/logoBTC.png"}
               ></IonImg>
-              <IonText>Bitcoin</IonText>
+              <IonText className="currencyName">Bitcoin</IonText>
             </IonItem>
             <IonItem color="none" lines="none">
               <IonSelect placeholder="Select a Currency" className="currencySelector">
-                <IonSelectOption value="Dollar">Dollars</IonSelectOption>
-                <IonSelectOption value="Cordoba">Cordobas</IonSelectOption>
+                <IonSelectOption value="Bitcoin">Bitcoin</IonSelectOption>
+                <IonSelectOption value="Etherium">Etherium</IonSelectOption>
               </IonSelect>
             </IonItem>
           </div>
@@ -51,21 +74,36 @@ const Home: React.FC = () => {
         <div className="homeSectionLine"></div>
         <IonImg className="homeGraph" src={"assets/Images/Graph.png"}></IonImg>
         <div className="homeSectionLine"></div>
+        <div className="ion-text-center ion-margin-top">
+          <IonText className="convertTitle">Prices</IonText>
+        </div>
+        <div className="valuesContainer">
+          <div className="revenueContainer">
+            <IonText>Currency</IonText>
+            <br />
+            <IonText className="revenueText"> BTC-USD</IonText>
+          </div>
+          <div className="priceContainer">
+            <IonText>Price (24h)</IonText>
+            <br />
+            <IonText className="revenueText"> $35000</IonText>
+          </div>
+        </div>
         <div className="valuesContainer">
           <div className="revenueContainer">
             <IonText>Miners Revenue</IonText>
             <br />
-            <IonText> $60000</IonText>
+            <IonText className="revenueText"> $60000</IonText>
           </div>
           <div className="priceContainer">
             <IonText>Bitcoin Price</IonText>
             <br />
-            <IonText> $35000</IonText>
+            <IonText className="revenueText"> $35000</IonText>
           </div>
         </div>
         <div className="homeSectionLine"></div>
         <div className="ion-text-center ion-margin-top">
-          <IonText>Convert</IonText>
+          <IonText className="convertTitle">Convert</IonText>
         </div>
         <div className="inputContainer">
           <IonItem color="none" lines="none">
