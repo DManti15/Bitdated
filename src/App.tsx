343ead559676771b5f3d412e6d05ad2ee25faaa5
DManti15 <dannymantigar@hotmail.com>
2023-07-22 20:00:37 -0600

    merge appRedesign to main

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet } from '@ionic/react';
+import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
+import Loading from './pages/Loading';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -22,15 +23,20 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+setupIonicReact();
+
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-        <Route exact path="/home">
-          <Home />
+        <Route exact path="/Loading">
+          <Loading />
         </Route>
         <Route exact path="/">
-          <Redirect to="/home" />
+          <Redirect to="/Loading" />
+        </Route>
+        <Route exact path="/home">
+          <Home />
         </Route>
       </IonRouterOutlet>
     </IonReactRouter>
